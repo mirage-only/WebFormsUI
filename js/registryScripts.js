048984905bbe8cdd4e4984232617dc45146e051f
Yauheni Valiantsinovich <zhenya.val.77@gmail.com>
2025-05-04 22:57:31 +0300

    refactoring registryScripts.js

--- js/registryScripts.js
+++ js/registryScripts.js
@@ -1,31 +1,39 @@
-document.getElementById('RegistrationForm').addEventListener('submit', async (event) => {
-    event.preventDefault();
+document
+    .getElementById('RegistrationForm')
+    .addEventListener('submit', async (event) =>
+    {
+        event.preventDefault();
 
-    const formData = {
-        name: document.getElementById('InputName').value,
-        surname: document.getElementById('InputSurname').value,
-        patronymic: document.getElementById('InputPatronymic').value,
-        email: document.getElementById('InputEmail').value,
-        passwordHash: document.getElementById('InputPassword1').value
-    };
+        const formData =
+        {
+                name: document.getElementById('InputName').value,
+                surname: document.getElementById('InputSurname').value,
+                patronymic: document.getElementById('InputPatronymic').value,
+                email: document.getElementById('InputEmail').value,
+                passwordHash: document.getElementById('InputPassword1').value
+        };
 
-    try {
-        const response = await fetch('https://localhost:44329/users/add', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(formData)
-        });
+        try
+        {
+            const response = await fetch('https://localhost:44329/users/add', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData)
+            });
 
-        if (!response.ok) {
-            throw new Error(`Ошибка: ${response.status}`);
+            if (!response.ok)
+            {
+                throw new Error(`Ошибка: ${response.status}`);
+            }
+
+            const result = await response.json();
+
+            alert('Регистрация прошла успешно!');
         }
-
-        const result = await response.json();
-
-        alert('Регистрация прошла успешно!');
-    } catch (error) {
-        alert('Произошла ошибка при регистрации.');
-    }
-});
+        catch (error)
+        {
+            alert('Произошла ошибка при регистрации.');
+        }
+    });
