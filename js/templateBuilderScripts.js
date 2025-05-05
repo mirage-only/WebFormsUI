26aa8b9ee54161028e83c98c80e8bbf2e497b319
Yauheni Valiantsinovich <zhenya.val.77@gmail.com>
2025-05-05 06:03:16 +0300

    1) update template builder

--- js/templateBuilderScripts.js
+++ js/templateBuilderScripts.js
@@ -1,20 +1,29 @@
+const inputPlaceHolder = "Enter question's name"
+
+function addElement(elementId, element)
+{
+        document
+            .getElementById(elementId)
+            .appendChild(element)
+}
+
 document
     .getElementById("addSmallTextQuestion")
     .addEventListener("click", () => {
 
         const newLabel = document.createElement("label");
-        newLabel.textContent = 'Short question:';
+        newLabel.textContent = `${document.querySelectorAll("#shortQuestionsContainer input").length + 1} short question:`;
         newLabel.classList.add("fw-bold");
 
         const newInput = document.createElement("input");
         newInput.type = "text";
-        newInput.name = `shortQuestion${document.querySelectorAll("#questionsContainer input").length + 1}`;
+        newInput.id = `shortQuestion${document.querySelectorAll("#shortQuestionsContainer input").length + 1}`;
         newInput.className = 'questionInput';
-        newInput.placeholder = "Enter question's name";
+        newInput.placeholder = inputPlaceHolder;
         newInput.required = true;
 
-        document.getElementById("questionsContainer").appendChild(newLabel);
-        document.getElementById("questionsContainer").appendChild(newInput);
+        addElement("shortQuestionsContainer", newLabel);
+        addElement("shortQuestionsContainer", newInput);
     });
 
 document
@@ -22,18 +31,18 @@ document
     .addEventListener("click", () => {
 
         const newLabel = document.createElement("label");
-        newLabel.textContent = 'Full text question:';
+        newLabel.textContent = `${document.querySelectorAll("#fullTextQuestionsContainer input").length + 1} full text question:`;
         newLabel.classList.add("fw-bold");
 
         const newInput = document.createElement("input");
         newInput.type = "text";
-        newInput.name = `fullTextQuestion${document.querySelectorAll("#questionsContainer input").length + 1}`;
+        newInput.id = `fullTextQuestion${document.querySelectorAll("#fullTextQuestionsContainer input").length + 1}`;
         newInput.className = 'questionInput';
-        newInput.placeholder = "Enter question's name";
+        newInput.placeholder = inputPlaceHolder;
         newInput.required = true;
 
-        document.getElementById("questionsContainer").appendChild(newLabel);
-        document.getElementById("questionsContainer").appendChild(newInput);
+        addElement("fullTextQuestionsContainer", newLabel);
+        addElement("fullTextQuestionsContainer", newInput);
     });
 
 document
@@ -41,22 +50,18 @@ document
     .addEventListener("click", () => {
 
         const newLabel = document.createElement("label");
-        newLabel.textContent = 'Non-negative integer question:';
+        newLabel.textContent = `${document.querySelectorAll("#nonNegativeIntegerQuestionsContainer input").length + 1} non-negative integer question:`;
         newLabel.classList.add("fw-bold");
 
         const newInput = document.createElement("input");
         newInput.type = "text";
-        newInput.name = 'nonNegIntQuestion';
+        newInput.id = `nonNegativeIntegerQuestion${document.querySelectorAll("#nonNegativeIntegerQuestionsContainer input").length + 1}`;
         newInput.className = 'questionInput';
-        newInput.placeholder = "Enter question's name";
+        newInput.placeholder = inputPlaceHolder;
         newInput.required = true;
 
-        document
-            .getElementById("questionsContainer")
-            .appendChild(newLabel);
-        document
-            .getElementById("questionsContainer")
-            .appendChild(newInput);
+        addElement("nonNegativeIntegerQuestionsContainer", newLabel);
+        addElement("nonNegativeIntegerQuestionsContainer", newInput);
     });
 
 document
@@ -64,23 +69,43 @@ document
     .addEventListener("click", () => {
 
         const newLabel = document.createElement("label");
-        newLabel.textContent = 'Question with responses options:';
+        newLabel.textContent = `${document.querySelectorAll("#withResponsesOptionsQuestionsContainer input").length + 1} question with responses options:`;
         newLabel.classList.add("fw-bold");
 
         const newInput = document.createElement("input");
         newInput.type = "text";
-        newInput.name = 'questionWithResponsesOptions';
+        newInput.id = `withResponsesOptionsQuestion${document.querySelectorAll("#withResponsesOptionsQuestionsContainer input").length + 1}`;
         newInput.className = 'questionInput';
-        newInput.placeholder = "Enter question's name";
+        newInput.placeholder = inputPlaceHolder;
         newInput.required = true;
 
-        document
-            .getElementById("questionsContainer")
-            .appendChild(newLabel);
-        document
-            .getElementById("questionsContainer")
-            .appendChild(newInput);
+        const newDivResponses = document.createElement("div");
+        newDivResponses.type = "div";
+        newDivResponses.id = `responsesOptionsContainer${document.querySelectorAll("#withResponsesOptionsQuestionsContainer input").length + 1}`;
+
+        const newInputResponseOption = document.createElement("input");
+
+
+        addElement("withResponsesOptionsQuestionsContainer", newLabel);
+        addElement("withResponsesOptionsQuestionsContainer", newInput);
+
+        if (document.querySelectorAll("#responsesOptionsContainer input").length === 0){
+            const newButton = document.createElement("button");
+            newButton.type = "button";
+            newButton.name = `addResponseOptionButton`;
+            newButton.textContent = `Add Response Option`;
+            newButton.className = 'btn btn-dark'
+            newButton.id = `addResponseOptionButton`;
+
+            addElement("withResponsesOptionsQuestionsContainer", newButton);
+        }
     });
+
+document
+    .getElementById("addResponseOptionButton")
+    .addEventListener("click", () => {
+
+    })
 
 
 
